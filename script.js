9255e0792a2fbdb48b03fcabdc0ec97cf0f4655a
FES001 <firdavs7794948@gmail.com>
2024-10-01 01:57:04 +0500

    Working on a todo list that uses localStorage of a browser.

---
+++ script.js
@@ -0,0 +1,3 @@
+const todoTextInput = document.getElementById('todoTextInput');
+const addButton = document.getElementById('addButton');
+const outputContainer = document.querySelector('.output-container');
